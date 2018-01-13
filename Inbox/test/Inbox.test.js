0d65f6d3d820fbb6a84457df99a6d35075211ac7
plamen1982 <plamen.hristov@ymail.com>
2018-01-13 13:54:54 -0500

    Fetching Accounts from Ganache

--- Inbox/test/Inbox.test.js
+++ Inbox/test/Inbox.test.js
@@ -1,38 +1,18 @@
-//assert is part of node and is used to check 2 values 
 const assert = require('assert');
 const ganache = require('ganache-cli');
-//Web3 is a constructor, that is why is with capital letter
 const Web3 = require('web3');
-
-//Every instance that we creating with web3 is to connect
-//with differnt ethereum network, ganache.provider() is 
-//instance of local ethereum network
 const web3 = new Web3(ganache.provider());
 
-class Car {
-    park() {
-        return 'stopped';
-    }
-
-    drive() {
-        return 'vroom';
-    }
-}
-//we define car here so we can have a global scope for the variable car and we can access from 
-//beforeEach() for new instance and from it for testing values
-let car;
-//befeoreEach is run with new instance every time before it statements
 beforeEach(() => {
-    car = new Car();
+    //Get a list of all accounts
+    web3.eth.getAccounts()
+    .then((fetchedAccounts) => {
+        console.log(fetchedAccounts)
+    })
+    //Use one of those accounts to deploy
+    //the contract
 });
-//the string Car is for our info for the test
-describe('Car', () => {
-    it('can park', () => {
-        //check if the outcome of car.park() is equal to 'stopped'
-        assert.equal(car.park(), 'stopped');
-    });
 
-    it('can drive', () => {
-        assert.equal(car.drive(), 'vroom')
-    });
+describe('Inbox', () => {
+    it('Deploys a contract', () => {})
 });
