de86370ca3e1d83c81095642fc86b41125f65743
“[plamen1982]” <plamen.hristov@ymail.com>
2018-01-29 14:09:24 -0500

    Styling, implemented videoDetails component wired. VideoList wired with VideoListItem for selectedVideo

--- ReactjsProject/src/components/video_list_item.js
+++ ReactjsProject/src/components/video_list_item.js
@@ -1,8 +1,20 @@
 import React from 'react';
 
-const VideoListItem = () => {
+const VideoListItem = ({ video, onVideoSelect }) => {
+    const imageUrl = video.snippet.thumbnails.default.url;
+    const title = video.snippet.title;
+
     return (
-        <li>Video</li>
+        <li onClick={()=> onVideoSelect(video)} className="list-group-item">
+            <div className="video-list media">
+                <div className="media-left">
+                    <img className="media-object" src={imageUrl}/>
+                </div>
+                <div className="media-body">
+                    <div className="media-heading">{title}</div>
+                </div>
+            </div>
+        </li>
     )
 }
 
--- ReactjsProject/src/index.js
+++ ReactjsProject/src/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyD1T478tOSu0FLehyIeSceHoRufHHdoJls';
 
@@ -13,10 +14,16 @@ class App extends Component {
   constructor(props) {
     super(props)
 
-    this.state = { videos: [] }
+    this.state = { 
+      videos: [], 
+      selectedVideo: null  
+    }
 
     YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-        this.setState({ videos })
+        this.setState({ 
+          videos: videos, 
+          selectedVideo: videos[0] 
+        })
     })
   }
 
@@ -25,7 +32,11 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
-        <VideoList videos = {this.state.videos} />
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos = {this.state.videos} 
+        />
       </div>
     );
   }
